8639161ac2890698cdbd8d208b93aa9baf4048a6
MiJey <yeaji21@gmail.com>
2017-05-25 16:19:28 +0900

    listview adapter 리스트뷰에 아이템추가하는 이벤트

--- app/src/main/java/mijey/kau_javaproject2017/MainActivity.java
+++ app/src/main/java/mijey/kau_javaproject2017/MainActivity.java
@@ -1,27 +1,43 @@
 package mijey.kau_javaproject2017;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "mijey.kau_javaproject2017.MESSAGE";
+
+    private ListView todoListview;
+    private ArrayAdapter<String> todoAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Android에서 제공하는 string 문자열 하나를 출력 가능한 layout으로 어댑터 생성
+        todoAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1);
+
+        // Xml에서 추가한 ListView 연결
+        todoListview = (ListView) findViewById(R.id.todolist);
+        todoListview.setAdapter(todoAdapter);
+        todoListview.setOnItemClickListener(onClickListItem);
+
     }
 
+    // 아이템 터치 이벤트
+    private AdapterView.OnItemClickListener onClickListItem = new AdapterView.OnItemClickListener() {
+
+        @Override
+        public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+            // 이벤트 발생 시 해당 아이템 위치의 텍스트를 출력
+        }
+    };
+
     public void addTask(View view) {
-        /*ListView todoList = (Listview)findViewById(R.id.todolist);
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-        */
+        todoAdapter.add("asdf");
     }
 }
