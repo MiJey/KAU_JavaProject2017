d4b5be9d498ff5da2f48ca184dbbca1a216a43aa
MiJey <yeaji21@gmail.com>
2017-05-25 11:56:32 +0900

    listview와 editText, button 레이아웃 설정

--- app/src/main/java/mijey/kau_javaproject2017/MainActivity.java
+++ app/src/main/java/mijey/kau_javaproject2017/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "mijey.kau_javaproject2017.MESSAGE";
@@ -14,11 +15,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void sendMessage(View view) {
+    public void addTask(View view) {
+        /*ListView todoList = (Listview)findViewById(R.id.todolist);
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+        */
     }
 }
